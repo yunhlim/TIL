42f7a90b1e95eb495a7e4728d9e7261e003c364a
yunh <yunh04131@gmail.com>
2022-01-15 21:56:40 +0900

    SWEA 6204

---
+++ algorithm/SWEA/training_startcamp/python_basic1/7th_6204_oper1.py
@@ -0,0 +1,9 @@
+# format 포맷팅
+# a = float(input()) 
+# b = a*2.54
+# print("{0:.2f} inch => {1} cm".format(a, b))
+
+# f-포맷팅
+a = float(input()) 
+b = a*2.54
+print("{0:.2f} inch => {1} cm".format(a, b))
