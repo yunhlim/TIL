b34220bcceae69ad5bb2df94e04048d1c17fc0cd
yunh <yunh04131@gmail.com>
2022-02-16 21:23:22 +0900

    little_modify

--- algorithm/Baekjoon/G4_1407/G4_1407.py
+++ algorithm/Baekjoon/G4_1407/G4_1407.py
@@ -6,5 +6,5 @@ while b >= 2:   # 더 이상 2의 배수가 없을 때까지
     b //= 2     # B보다 작은 2의 거듭제곱으로 나누어주기 위해 계속 나누며 확인한다.
     exp += 1    # 2의 지수를 1 더한다.
     two_exp = 2**exp    # 2의 거듭제곱 값
-    sum += (B//two_exp - (A-1)//two_exp) * (two_exp//2)
+    sum += (B//two_exp - (A-1)//two_exp) * (two_exp//2) # [A, B] 영역에서 배수의 개수에 2의 거듭제곱의 절반을 곱한다.
 print(sum)
