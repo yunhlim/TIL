9f1cc7267b90792d1de6721957e6c424223e089d
yunh <yunh04131@gmail.com>
2022-02-18 15:15:27 +0900

    SWEA_1267

--- algorithm/SWEA/D6_1267/D6_1267.py
+++ algorithm/SWEA/D6_1267/D6_1267.py
@@ -1,6 +1,5 @@
 from collections import deque
-import sys
-sys.stdin = open("input.txt")
+
 for tc in range(1, 11):
     V, E = map(int, input().split())
     # 정점(인덱스) => 정점들의 집합(set) , 정점(인덱스) <= 정점들의 집합(set)
@@ -22,66 +21,3 @@ for tc in range(1, 11):
             queue += graph_lst[0][node] - set(visited) # 정점에서 연결된 정점들 중 아직 나오지 않는 정점들만 큐에 담는다.
     print(f'#{tc} ', end='')
     print(*visited)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import sys
-# sys.stdin = open("input.txt")
-# T = 10
-# for tc in range(1, 1+T):
-#     V, E = map(int, input().split())
-
-#     arr = [[0 for _ in range(V+1)] for _ in range(V+1)]   # padding을 더한 2차원 배열
-#     visited = [0 for _ in range(V+1)]   # 등장한 간선인지 선택
-#     result = []
-#     in_lst = list(map(int,input().split()))
-#     for i in range(E):               # 배열에 담아준다.
-#         arr[in_lst[2*i]][in_lst[2*i+1]] = 1
-
-#     for i in range(1, V+1):
-#         for j in range(1, V+1):
-#             if arr[j][i]:  # 방향선이 꽂히는 정점은 break
-#                 break
-#         else:
-#             visited[i] = 1
-#             result.append(i)
-#             nows = [i]       
-#             while nows != []:
-#                 new_nows = []
-#                 for now in nows:
-#                     for k in range(1, V+1):
-#                         if arr[now][k] and visited[k] == 0:   # 방향선을 받는 새로운 정점
-#                             for l in range(1, V+1):
-#                                 if arr[l][k] and visited[l] == 0:
-#                                     break
-#                             else:
-#                                 visited[k] = 1
-#                                 result.append(k)
-#                                 new_nows.append(k)
-#                 nows = new_nows[:]
-#         if len(result) == V:
-#             break
-#     print(f'#{tc} ', end='')
-#     print(*result)
-
-            
