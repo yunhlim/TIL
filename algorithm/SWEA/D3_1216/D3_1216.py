799c4ae0a21cdddb41361e486bc38e90d1a8cf95
yunh <yunh04131@gmail.com>
2022-02-20 19:14:26 +0900

    func_modified

--- algorithm/SWEA/D3_1216/D3_1216.py
+++ algorithm/SWEA/D3_1216/D3_1216.py
@@ -1,3 +1,16 @@
+def pallindrome():
+    for m in range(100,0,-1):   # m은 회문의 길이
+        # row의 회문 탐색
+        for i in range(100):
+            for j in range(100-m+1):
+                if arr[i][j:j+m] == arr[i][j:j+m][::-1]:
+                    print(f'#{tc} {m}')
+                    return
+                elif arr_t[i][j:j + m] == arr_t[i][j:j + m][::-1]:
+                    print(f'#{tc} {m}')
+                    return
+
+
 for _ in range(10):
     tc = int(input())
     arr = [input() for _ in range(100)]     # 입력받은 2차원 배열
@@ -6,17 +19,4 @@ for _ in range(10):
     for i in range(100):
         for j in range(100):
             arr_t[i][j] = arr[j][i]
-
-    def pallindrome():
-        for m in range(100,0,-1):   # m은 회문의 길이
-            # row의 회문 탐색
-            for i in range(100):
-                for j in range(100-m+1):
-                    if arr[i][j:j+m] == arr[i][j:j+m][::-1]:
-                        print(f'#{tc} {m}')
-                        return
-                    elif arr_t[i][j:j + m] == arr_t[i][j:j + m][::-1]:
-                        print(f'#{tc} {m}')
-                        return
-
     pallindrome()
