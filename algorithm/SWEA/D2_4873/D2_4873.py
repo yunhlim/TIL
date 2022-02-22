bd255cdeec969e3fa199e2ead07da63513bc961a
yunh <yunh04131@gmail.com>
2022-02-22 14:36:25 +0900

    SWEA_4873

---
+++ algorithm/SWEA/D2_4873/D2_4873.py
@@ -0,0 +1,12 @@
+# 반복문자 지우기
+T = int(input())
+for tc in range(1, T+1):
+    arr = list(input())
+    s = 0
+    while s < len(arr): # 이전 값과 비교하여 없애고 없애면 하나 전 인덱스로 이동해 다시 확인
+        if s and arr[s] == arr[s-1]:    # s가 0이 아닐 때 이전 값과 비교
+            del arr[s], arr[s-1]    # 같을 땐 값을 지우고 이전 인덱스로 이동해 비교한다.
+            s -= 1
+        else:   # 다를 땐 다음 인덱스로 이동한다.
+            s += 1
+    print(f'#{tc} {len(arr)}')
