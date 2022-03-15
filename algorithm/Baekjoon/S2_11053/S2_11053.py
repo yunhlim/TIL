fd5aa4ea2321e4be95c0feefaceee6ac2c1bfc05
yunh <yunh04131@gmail.com>
2022-03-16 00:49:34 +0900

    BOJ_11053

---
+++ algorithm/Baekjoon/S2_11053/S2_11053.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [1 for _ in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], 1 + dp[j])
+            
+print(max(dp))
