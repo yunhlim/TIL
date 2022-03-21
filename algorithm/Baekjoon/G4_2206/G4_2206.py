ceba025606ca51b2b0808aca50454b17b85e1141
yunh <yunh04131@gmail.com>
2022-03-21 23:58:36 +0900

    BOJ 2206 clear

--- algorithm/Baekjoon/G4_2206/G4_2206.py
+++ algorithm/Baekjoon/G4_2206/G4_2206.py
@@ -1,8 +1,9 @@
+import sys
 from collections import deque
-
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = [list(input()) for _ in range(n)]
+arr = [list(map(int, input().rstrip())) for _ in range(n)]
 INF = n * m
 dy = [0, 1, 0, -1]
 dx = [1, 0, -1, 0]
@@ -11,27 +12,27 @@ queue = deque()
 queue.append((0, 0))
 visited[0][0] = [1, 0]
 
-while queue and visited[n-1][m-1][0] == INF:
+while queue and visited[n-1][m-1][1] == 2:
     y, x = queue.popleft()
-    print(y, x)
     for i in range(4):
         ny = y + dy[i]
         nx = x + dx[i]
         if not(0 <= ny < n and 0 <= nx < m):
             continue
-        if arr[ny][nx] == 0:
-            print(ny, nx)   
+        if arr[ny][nx] == 0:  
             if visited[y][x][1] < visited[ny][nx][1]:
-                visited[ny][nx] = [visited[y][x][0] + 1, 0]
+                visited[ny][nx] = [visited[y][x][0] + 1, visited[y][x][1]]
                 queue.append((ny, nx))
             elif visited[y][x][1] == visited[ny][nx][1]:
                 if visited[y][x][0] + 1 < visited[ny][nx][0]:
                     visited[ny][nx] = [visited[y][x][0] + 1, visited[y][x][1]]
                     queue.append((ny, nx))
         else:
-            if visited[y][x][1] == 0:
-                if visited[y][x][0] + 1 < visited[ny][nx][0]:
-                    visited[ny][nx] = [visited[y][x][0] + 1, 1]
-                    queue.append((ny, nx))
-print(visited)
-print(visited[n-1][m-1])
+            if visited[y][x][1] == 0 and (visited[y][x][0] + 1 < visited[ny][nx][0]):
+                visited[ny][nx] = [visited[y][x][0] + 1, 1]
+                queue.append((ny, nx))
+
+if visited[n-1][m-1][1] == 2:
+    print(-1)
+else:
+    print(visited[n-1][m-1][0])
