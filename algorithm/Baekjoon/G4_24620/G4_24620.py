1ecd659af2136864de548999c4b14ae7471c25c4
yunh <yunh04131@gmail.com>
2022-03-28 00:23:14 +0900

    BOJ 24620

--- algorithm/Baekjoon/G4_24620/G4_24620.py
+++ algorithm/Baekjoon/G4_24620/G4_24620.py
@@ -1,18 +1,19 @@
-def search(x):
-    remember = 0
+def check(x):   # 매개변수 탐색, x가 가능한지 확인
+    total = 0
     for i in range(n):
-        if remember > x:
+        if total > x:
             return False
-        remember += arr[i]
-        if remember == x:
-            remember = 0
+        total += arr[i]
+        if total == x:
+            total = 0
     return True
+
 
 for _ in range(int(input())):
     n = int(input())
     arr = list(map(int, input().split()))
     mmax = 0
-    ssum = 0
+    total = 0
     zero_cnt = 0
 
     for i in range(n):      # 최댓값, 최솟값 구하기
@@ -20,22 +21,11 @@ for _ in range(int(input())):
             mmax = arr[i]
         if arr[i] == 0:
             zero_cnt += 1
-        ssum += arr[i]
+        total += arr[i]
 
-    arr2 = []
-
-    if mmax == 0:
-        print(0)
-    else:
-        for i in range(1, ssum + 1):
-            if i * i > ssum:                   # 제곱근 이하까지만 계산
+    ans = 0
+    for i in range(max(1, mmax), total + 1):    # 최댓값 이상부터 가능하니 그 때부터 체크
+        if total % i == 0 and check(i):         # 나누어떨어지면 그 수로 가능한지 확인
+                ans = n - total // i
                 break
-            if ssum % i == 0:
-                arr2.append(i)
-                if i * i != ssum:              # 제곱수인지 판단
-                    arr2.append(ssum // i)
-        arr2.sort()         # 순서대로 정렬
-        for v in arr2:
-            if search(v) and mmax <= v:
-                print(n - ssum // v)
-                break
+    print(ans)
