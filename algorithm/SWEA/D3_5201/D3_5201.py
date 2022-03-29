58feb5f14247284d722e9e11a5719881a544b253
yunh <yunh04131@gmail.com>
2022-03-29 15:05:43 +0900

    SWEA 5201

--- algorithm/SWEA/D3_5201/D3_5201.py
+++ algorithm/SWEA/D3_5201/D3_5201.py
@@ -1,17 +1,16 @@
 for tc in range(1, int(input()) + 1):
-    n, m = map(int, input().split())    # n:컨테이너의 개수 m:
+    n, m = map(int, input().split())
     containers = list(map(int, input().split()))
     trucks = list(map(int, input().split()))
     containers.sort(reverse=True)
     trucks.sort(reverse=True)
+    c, t = 0, 0     # 컨테이너와 트럭을 큰 것부터 확인
     total = 0       # 총 무게
 
-    s = 0
-    for i in range(m):
-        for j in range(s, n):
-            if trucks[i] >= containers[j]:  # 담을 수 있는 경우
-                total += containers[j]      # 무게를 더한다
-                s = j + 1                   # 다음 컨테이너 시작점을 지정
-                break
+    while t < m and c < n:  # 컨테이너나 트럭이 인덱스를 초과하면 종료
+        if trucks[t] >= containers[c]:  # 담을 수 있는 경우
+            total += containers[c]
+            t += 1                      # 다음 트럭을 담는다.
+        c += 1                          # 컨테이너는 항상 다음으로
 
     print(f'#{tc} {total}')
