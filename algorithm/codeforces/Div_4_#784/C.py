1c63889e0710907f922d906281238c347d8d75fd
yunh <yunh04131@gmail.com>
2022-05-07 22:45:21 +0900

    codeforces Div4 #784

--- algorithm/codeforces/Div_4_#784/C.py
+++ algorithm/codeforces/Div_4_#784/C.py
@@ -2,8 +2,8 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     arr = list(map(int, input().split()))
+    
     for i in range(2, n):
-
         if arr[i % 2] % 2 != arr[i] % 2:
                 print('NO')
                 break
--- algorithm/codeforces/Div_4_#784/H.py
+++ algorithm/codeforces/Div_4_#784/H.py
@@ -2,9 +2,16 @@ t = int(input())
 for _ in range(t):
     n, k = map(int, input().split())
     arr = list(map(int, input().split()))
+    count = [0 for _ in range(31)]
+
+    for i in range(n):
+        for j in range(31):
+            if arr[i] & (1 << j):
+                count[j] += 1
+    
     result = 0
-    visited = [0 for _ in range(30)]
-    # 비트를 visited에 넣어준다.
-    # 비트마스킹
-    for i in range(30):
-        pass
+    for i in range(31)[::-1]:
+        if count[i] + k >= n:
+            result += 1 << i
+            k -= n - count[i]
+    print(result)
