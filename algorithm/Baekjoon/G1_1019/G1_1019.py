17575a2b08172de1a1c39c7fbe5b4c5a114fbcae
SSAFY <yunh04131@gmail.com>
2022-07-18 12:18:40 +0900

    BOJ 1019

--- algorithm/Baekjoon/G1_1019/G1_1019.py
+++ algorithm/Baekjoon/G1_1019/G1_1019.py
@@ -1,7 +1,18 @@
+def page(digit, num):
+    nameji = num % 10
+    mok = num // 10
+    
+    if mok:
+        page(digit + 1, mok)
+
+
 n = int(input())
 visited = [0 for _ in range(10)]
+
+visited_sol = [0 for _ in range(10)]
 for i in range(1, n + 1):
     for c in str(i):
-        visited[int(c)] += 1
+        visited_sol[int(c)] += 1
+print(*visited_sol)
 
-print(*visited)
+digit = 1
