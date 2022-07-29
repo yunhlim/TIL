7b4011d75f35de4108b06f94e99ede192be70ff1
SSAFY <yunh04131@gmail.com>
2022-07-30 02:39:11 +0900

    BOJ 2504 ing..

--- algorithm/Baekjoon/S1_2504/S1_2504.py
+++ algorithm/Baekjoon/S1_2504/S1_2504.py
@@ -2,10 +2,18 @@ string = input()
 stack = []
 calc = []
 result = 0
-
+tmp = 1
+prv = -1         # 이전 값이 닫는 괄호였으면 0, 여는 괄호였으면 1
 for c in string:
-    if c == '(':
+    if c in '([':
         stack.append(c)
-    if c == ')':
+    elif c in ')]':
+        if not stack:       # 닫는 괄호가 나왔는데 stack에 여는 괄호가 없다면 0 출력
+            result = 0
+            break
         x = stack.pop()
-        if 
+        if c == ')':
+            if not prv:     # 닫는 괄호였을 경우
+                pass
+        if c == ']':
+            pass
